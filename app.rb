a1a18347763c7dff9e8c0c1c04a43271a6f27ddd
iL53n <ilsegribko@gmail.com>
2021-03-17 19:18:46 +0300

    add create ad method

--- app.rb
+++ app.rb
@@ -3,18 +3,33 @@ require 'sinatra/activerecord'
 require 'fast_jsonapi'
 require 'pg'
 require 'rack'
+require 'json'
 
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 Dir["#{current_dir}/serializers/*.rb"].each { |file| require file }
 
+before do
+  content_type :json
+end
+
 get "/" do
   ads = Ad.order(updated_at: :desc)
   serializer = AdSerializer.new(ads)
 
-  serializer.serialized_json
+  body serializer.serialized_json
+  status 200
 end
 
-post "/abs" do
-  @ad = Ad.create(params[:title, :description, :city])
+post "/ads" do
+  ad = Ad.new(params)
+
+  if ad.save
+    serializer = AdSerializer.new(ad)
+
+    body serializer.serialized_json
+    status 201
+  else
+    halt 422, "You can't save new ad"
+  end
 end
