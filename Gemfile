09ea23bbf7cea0b079fea1d7b4535d0b4e57be95
iL53n <ilsegribko@gmail.com>
2021-03-17 18:08:46 +0300

    add ad serializer

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.7.0'
 
+gem 'fast_jsonapi', '~> 1.5'
 gem 'pg', '~> 1.2'
 gem 'rake', '~> 13.0'
 gem 'sinatra', '~> 2.1'
--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'fast_jsonapi'
 require 'pg'
+require 'rack'
 
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
+Dir["#{current_dir}/serializers/*.rb"].each { |file| require file }
 
 get "/" do
-  @ads = Ad.all.to_json
+  ads = Ad.order(updated_at: :desc)
+  serializer = AdSerializer.new(ads)
+
+  serializer.serialized_json
 end
 
 post "/abs" do
