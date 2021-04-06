d5180a2397665356a8d3f4d3f451699f667b355d
iL53n <ilsegribko@gmail.com>
2021-04-06 19:31:59 +0300

    test(app): add todo in geocoding spec

---
+++ spec/lib/geocoding_service/client_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe GeocodingService::Client, type: :client do
+  subject { described_class.new(connection: connection) }
+
+  # Todo: add tests
+  # let(:status) { 200 }
+  # let(:headers) { { 'Content-Type' => 'application/json'} }
+  # let(:body) { {} }
+  #
+  # before do
+  #   stubs.get('/') { [status, headers, body.to_json] }
+  # end
+  #
+  # describe '#geocode (city exists)' do
+  #   let(:coordinates) { [45.2, 90.6].to_json }
+  #
+  #   it 'return coordinates' do
+  #     expect(subject.geocode('City')).to eq(coordinates)
+  #   end
+  # end
+  #
+  # describe '#geocode (city doesn\'t exist)' do
+  #   let(:status) { 403 }
+  #
+  #   it 'returns nil' do
+  #     expect(subject.geocode('invalid.city')).to be_nil
+  #   end
+  # end
+  #
+  # describe '#geocode (city is nil)' do
+  #   let(:status) { 403 }
+  #
+  #   it 'returns nil' do
+  #     expect(subject.geocode(nil)).to be_nil
+  #   end
+  # end
+end
