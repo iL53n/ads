a716f259fb736e11ae80916d724d0b9d37e236e0
iL53n <ilsegribko@gmail.com>
2021-04-22 19:09:16 +0300

    feat(app): auth_implemented through rpc

--- app/lib/auth_service/client.rb
+++ app/lib/auth_service/client.rb
@@ -6,16 +6,51 @@ module AuthService
     extend Dry::Initializer[undefined: false]
     include Api
 
-    option :url, default: proc { 'http://localhost:3010/v1' }
-    option :connection, default: proc { build_connection }
+    # option :url, default: proc { 'http://localhost:3010/v1' }
+    # option :connection, default: proc { build_connection }
+    option :channel, default: proc { RabbitMQ.channel }
+    option :exchange, default: proc { channel.default_exchange }
+    option :reply_queue, default: proc { create_reply_queue }
+    option :lock, default: proc { Mutex.new }
+    option :condition, default: proc { ConditionVariable.new }
+    option :response, default: proc { set_response }
+
+    def publish(payload, opts = {})
+      @lock.synchronize do
+        self.correlation_id = SecureRandom.uuid
+
+        @exchange.publish(
+          payload,
+          opts.merge(
+            routing_key: 'auth',
+            correlation_id: @correlation_id,
+            reply_to: @reply_queue.name
+          )
+        )
+
+        @condition.wait(@lock)
+      end
+
+      @response
+    end
 
     private
 
-    def build_connection
-      Faraday.new(@url) do |conn|
-        conn.request :json
-        conn.response :json, content_type: /\bjson$/
-        conn.adapter Faraday.default_adapter
+    attr_writer :response, :correlation_id
+
+    def create_reply_queue
+      @channel.queue('', exclusive: true)
+    end
+
+    def set_response
+      self.correlation_id = SecureRandom.uuid
+
+      @reply_queue.subscribe do |delivery_info, properties, payload|
+        if properties[:correlation_id] == @correlation_id
+          self.response = JSON(payload)['user_id']
+
+          @lock.synchronize { @condition.signal }
+        end
       end
     end
   end
--- spec/factories/ads.rb
+++ spec/factories/ads.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     title { 'Ad title' }
     description { 'Ad description' }
     city { 'City' }
+    lat { 11 }
+    lon { 22 }
     user_id { 101 }
   end
 end
